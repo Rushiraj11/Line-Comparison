0391d7f5243df9e192a4b93e5ace2419d38d2661
Rushiraj11 <grushiraj95@gmail.com>
2021-08-27 21:16:16 +0530

    Added : Lenght of line using Cartesian Coordinates

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class LineComparison {
     public static void main (String [] args) {
-        System.out.println("Welcome to Line Comparison Computation program");
+        //calling Scanner method to take inputs from console
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Cartesian Co-ordinates");
+        System.out.println("Enter x1 ");
+       int x1 = sc.nextInt();
+        System.out.println("Enter x2 ");
+       int x2 = sc.nextInt();
+        System.out.println("Enter y1 ");
+       int y1 = sc.nextInt();
+        System.out.println("Enter y2 ");
+       int y2 = sc.nextInt();
+        System.out.println("Length of the line:"+(Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1),2)));
     }
 }
